c374074a909452f30d2d3ecd010dcfaa5edb43b2
JayParikh39 <jayparikh39@gmail.com>
2025-11-13 12:53:13 -0500

    Bug Fixes

--- lancer_project/urls.py
+++ lancer_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,3 +11,8 @@ urlpatterns = [
     path('tracking/', include('injury_tracking.urls', namespace='tracking')),
     path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
